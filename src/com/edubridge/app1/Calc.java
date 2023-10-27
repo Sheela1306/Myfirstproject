5d2d91f02af526151c723b20e0f18cd4cbc209d9
Sheela1306 <146758721+Sheela1306@users.noreply.github.com>
2023-10-27 16:17:38 +0530

    Update Calc.java

--- src/com/edubridge/app1/Calc.java
+++ src/com/edubridge/app1/Calc.java
@@ -4,7 +4,20 @@ public class Calc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+public static void  findSquare(int num) {
+		System.out.println(num*num);
+	}
+		
+	public static void  findCube(int num) {
+			System.out.println(num*num*num);
+
+
+		
+		
+		
 
 	}
 
 }
+
+
